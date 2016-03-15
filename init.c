135c0ddc0e93bb118b93eb35f0b52e834b81d3f0
Punit Vara <punitvara@gmail.com>
2016-03-16 02:09:43 +0530

    add copyright

--- init.c
+++ init.c
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) 2016 Punit Vara <punitvara@gmail.com>
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.org/license/LICENSE.
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
