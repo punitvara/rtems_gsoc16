2c1efe8d9373c77fc991e0a7018e642df384520b
Punit Vara <punitvara@gmail.com>
2016-07-04 18:50:58 +0530

    add README and update pwm.c

--- pwm_final2/pwm.c
+++ pwm_final2/pwm.c
@@ -25,6 +25,12 @@
 #include <bsp.h>
 #include <bsp/bbb-pwm.h>
 
+/* Currently these definitions are for BeagleBone Black board only
+ * Later on Beagle-xM board support can be added in this code.
+ * After support gets added if condition should be removed
+ */
+#if IS_AM335X
+
 /*
  * @brief This function select PWM module to be enabled
  * 
@@ -32,70 +38,59 @@
  * 
  * @return Base Address of respective pwm instant.
 */
-
-static uint32_t select_pwmss(uint32_t pwm_id)
+static uint32_t select_pwm(BBB_PWMSS pwm_id)
 {
 uint32_t baseAddr=0;
-   if (pwm_id == BBBIO_PWMSS0)
-   {
-	baseAddr = EPWM_0_REGS;
-	return baseAddr;
-   }
-   else if (pwm_id == BBBIO_PWMSS1)
-   {
-	baseAddr = EPWM_1_REGS;
-	return baseAddr;
-   } 
-   else if (pwm_id == BBBIO_PWMSS2)
-   {
-	baseAddr = EPWM_2_REGS;
-	return baseAddr;
-   }
+   if (pwm_id == BBB_PWMSS0)
+	baseAddr = AM335X_EPWM_0_REGS;
+   else if (pwm_id == BBB_PWMSS1)
+	baseAddr = AM335X_EPWM_1_REGS;
+   else if (pwm_id == BBB_PWMSS2)
+	baseAddr = AM335X_EPWM_2_REGS;
    else 
-   {
-	printf("Invalid PWM Id\n");
-	return 0;	
-   }
+	baseAddr = 0;
+return baseAddr;	
 }
 
-/**
- * @brief   This function Enables pinmuxing for PWM module.
- *          
- *
- * @param   pwm_id  It is the instance number of EPWM of pwmsubsystem.
- *
- *
- * @return None
- **/
-
-static void epwm_pinmux_setup(uint32_t pwm_id)
+bool beagle_pwm_pinmux_setup(uint32_t pin_no, BBB_PWMSS pwm_id)
 {
-  switch(pwm_id)  {
-	case EPWM_GROUP2:
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_GPMC_AD(9)) = BBB_MUXMODE(MUXMODE4);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_GPMC_AD(8)) = BBB_MUXMODE(MUXMODE4);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_LCD_DATA(0)) = BBB_MUXMODE(MUXMODE3);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_LCD_DATA(1)) = BBB_MUXMODE(MUXMODE3);
-		printf("Pinmux mode on \n");
-		break;
-	case EPWM_GROUP1:
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_LCD_DATA(11)) = BBB_MUXMODE(MUXMODE2);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_LCD_DATA(10)) = BBB_MUXMODE(MUXMODE2);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_GPMC_AD(2)) = BBB_MUXMODE(MUXMODE6);
-		REG(AM335X_PADCONF_BASE + CONTROL_CONF_GPMC_AD(3)) = BBB_MUXMODE(MUXMODE6);
-		break;
-	case EPWM_GROUP0:
-		REG(AM335X_PADCONF_BASE + AM335X_CONF_SPI0_D0) = BBB_MUXMODE(MUXMODE3);
-		REG(AM335X_PADCONF_BASE + AM335X_CONF_SPI0_SCLK) = BBB_MUXMODE(MUXMODE3);
-		REG(AM335X_PADCONF_BASE + AM335X_CONF_MCASP0_FSX) = BBB_MUXMODE(MUXMODE1);
-		REG(AM335X_PADCONF_BASE + AM335X_CONF_MCASP0_ACLKX) = BBB_MUXMODE(MUXMODE1);
-		break;
-	
-	default:
-		printf("PWM output is not available on this pin\n");
-		break;
-}
-
+bool is_valid = true;
+  if(pwm_id == BBB_PWMSS0) {
+	if (pin_no == BBB_P9_21_0B)
+	  REG(AM335X_PADCONF_BASE + AM335X_CONF_SPI0_D0) = BBB_MUXMODE(BBB_P9_21_MUX_PWM);
+	else if (pin_no == BBB_P9_22_0A)
+	  REG(AM335X_PADCONF_BASE + AM335X_CONF_SPI0_SCLK) = BBB_MUXMODE(BBB_P9_22_MUX_PWM);
+	else if (pin_no == BBB_P9_29_0B)
+	  REG(AM335X_PADCONF_BASE + AM335X_CONF_MCASP0_FSX) = BBB_MUXMODE(BBB_P9_29_MUX_PWM);
+	else if (pin_no == BBB_P9_31_0A)
+	  REG(AM335X_PADCONF_BASE + AM335X_CONF_MCASP0_ACLKX) = BBB_MUXMODE(BBB_P9_31_MUX_PWM);
+	else 
+	  is_valid = false;
+  } else if (pwm_id == BBB_PWMSS1) {
+	if (pin_no == BBB_P8_34_1B)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_LCD_DATA(11)) = BBB_MUXMODE(BBB_P8_34_MUX_PWM);
+	else if (pin_no == BBB_P8_36_1A)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_LCD_DATA(10)) = BBB_MUXMODE(BBB_P8_36_MUX_PWM);
+	else if (pin_no == BBB_P9_14_1A)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_GPMC_AD(2)) = BBB_MUXMODE(BBB_P9_14_MUX_PWM);
+	else if (pin_no == BBB_P9_16_1B)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_GPMC_AD(3)) = BBB_MUXMODE(BBB_P9_14_MUX_PWM);
+	else 
+	  is_valid = false;
+  } else if (pwm_id == BBB_PWMSS2) {
+	if (pin_no == BBB_P8_13_2B)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_GPMC_AD(9)) = BBB_MUXMODE(BBB_P8_13_MUX_PWM);
+	else if (pin_no == BBB_P8_19_2A)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_GPMC_AD(8)) = BBB_MUXMODE(BBB_P8_19_MUX_PWM);
+	else if (pin_no == BBB_P8_45_2A)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_LCD_DATA(0)) = BBB_MUXMODE(BBB_P8_45_MUX_PWM);
+	else if (pin_no == BBB_P8_46_2B)
+	  REG(AM335X_PADCONF_BASE + BBB_CONTROL_CONF_LCD_DATA(1)) = BBB_MUXMODE(BBB_P8_46_MUX_PWM);
+	else
+	  is_valid = false;
+  } else 
+	is_valid = false;
+  return is_valid;
 }
 
 /**
@@ -106,23 +101,22 @@ static void epwm_pinmux_setup(uint32_t pwm_id)
  *
  * @return  true if successful
  **/
-
-static bool pwmss_tbclk_enable(unsigned int instance)
+static bool pwmss_tbclk_enable(BBB_PWMSS instance)
 {
 uint32_t enable_bit;
 bool is_valid = true;
   
-  if (instance == BBBIO_PWMSS0)
+  if (instance == BBB_PWMSS0)
   {
-    	enable_bit = BBBIO_PWMSS_CTRL_PWMSS0_TBCLKEN;
+    	enable_bit = AM335X_PWMSS_CTRL_PWMSS0_TBCLKEN;
   }
-  else if (instance == BBBIO_PWMSS1)
+  else if (instance == BBB_PWMSS1)
   {
-     	enable_bit = BBBIO_PWMSS_CTRL_PWMSS1_TBCLKEN;
+     	enable_bit = AM335X_PWMSS_CTRL_PWMSS1_TBCLKEN;
   }
-  else if (instance == BBBIO_PWMSS2)
+  else if (instance == BBB_PWMSS2)
   {
-     	enable_bit = BBBIO_PWMSS_CTRL_PWMSS2_TBCLKEN;
+     	enable_bit = AM335X_PWMSS_CTRL_PWMSS2_TBCLKEN;
   }
   else
   {
@@ -131,7 +125,7 @@ bool is_valid = true;
 
   if (is_valid)
   {
-    	REG(AM335X_PADCONF_BASE + BBBIO_PWMSS_CTRL) |= enable_bit;
+    	REG(AM335X_PADCONF_BASE + AM335X_PWMSS_CTRL) |= enable_bit;
   }
 
   return is_valid;
@@ -145,12 +139,17 @@ bool is_valid = true;
  * @return  None.
  *
  **/
-
-static void epwm_clock_enable(uint32_t pwm_id)
-{	
-	uint32_t baseAddr;
-	baseAddr = select_pwmss(pwm_id);
-        REG(baseAddr - BBBIO_EPWM_REGS + PWMSS_CLKCONFIG) |= PWMSS_CLK_EN_ACK;
+static bool pwm_clock_enable(BBB_PWMSS pwm_id)
+{
+  const bool id_is_valid = pwm_id < BBB_PWMSS_COUNT;	
+  bool status = true;
+  if(id_is_valid) {
+    const uint32_t baseAddr = select_pwm(pwm_id);
+    REG(baseAddr - AM335X_EPWM_REGS + AM335X_PWMSS_CLKCONFIG) |= AM335X_PWMSS_CLK_EN_ACK;
+  } else {
+    status = false;
+  }
+  return status;
 }
 
 /**
@@ -167,125 +166,108 @@ static void epwm_clock_enable(uint32_t pwm_id)
  * @return  None.
  *
  */
-
-static void module_clk_config(uint32_t pwmss_id)
+static bool module_clk_config(BBB_PWMSS pwmss_id)
 {
-        if(pwmss_id == 0)
-        {
-                REG(BBBIO_CM_PER_ADDR + BBBIO_CM_PER_EPWMSS0_CLKCTRL) |=
-                        BBBIO_CM_PER_EPWMSS0_CLKCTRL_MODULEMODE_ENABLE;
-        }
-        else if(pwmss_id == 1)
-        {
-                REG(BBBIO_CM_PER_ADDR + BBBIO_CM_PER_EPWMSS1_CLKCTRL) |=
-                        BBBIO_CM_PER_EPWMSS1_CLKCTRL_MODULEMODE_ENABLE;
-        }
-        else if(pwmss_id == 2)
-        {
-                REG(BBBIO_CM_PER_ADDR + BBBIO_CM_PER_EPWMSS2_CLKCTRL) |=
-                        BBBIO_CM_PER_EPWMSS2_CLKCTRL_MODULEMODE_ENABLE;
-        }
-        else
-        {
-		printf("Please enter valid pwm Id \n");
-        }
+  bool is_valid = true;
+  if(pwmss_id == BBB_PWMSS0) {
+    const uint32_t is_functional = AM335X_CM_PER_EPWMSS0_CLKCTRL_IDLEST_FUNC <<
+                         AM335X_CM_PER_EPWMSS0_CLKCTRL_IDLEST_SHIFT;
+    const uint32_t clkctrl = AM335X_CM_PER_ADDR + AM335X_CM_PER_EPWMSS0_CLKCTRL;
+    const uint32_t idle_bits = AM335X_CM_PER_EPWMSS0_CLKCTRL_IDLEST;
+    const uint32_t is_enable = AM335X_CM_PER_EPWMSS0_CLKCTRL_MODULEMODE_ENABLE;
+    const uint32_t module_mode = AM335X_CM_PER_EPWMSS0_CLKCTRL_MODULEMODE;
+    
+    REG(clkctrl) |= is_enable;
+    while((REG(clkctrl) & module_mode) != is_enable);
+    while((REG(clkctrl) & idle_bits) != is_functional);
+    }
+    else if(pwmss_id == BBB_PWMSS1) {
+    const uint32_t is_functional = AM335X_CM_PER_EPWMSS1_CLKCTRL_IDLEST_FUNC <<
+                         AM335X_CM_PER_EPWMSS1_CLKCTRL_IDLEST_SHIFT;
+    const uint32_t clkctrl = AM335X_CM_PER_ADDR + AM335X_CM_PER_EPWMSS1_CLKCTRL;
+    const uint32_t idle_bits = AM335X_CM_PER_EPWMSS1_CLKCTRL_IDLEST;
+    const uint32_t is_enable = AM335X_CM_PER_EPWMSS1_CLKCTRL_MODULEMODE_ENABLE;
+    const uint32_t module_mode = AM335X_CM_PER_EPWMSS1_CLKCTRL_MODULEMODE;
+
+    REG(clkctrl) |= is_enable;
+    while((REG(clkctrl) & module_mode) != is_enable);
+    while((REG(clkctrl) & idle_bits) != is_functional);
+    } else if(pwmss_id == BBB_PWMSS2) {
+    const uint32_t is_functional = AM335X_CM_PER_EPWMSS2_CLKCTRL_IDLEST_FUNC <<
+                         AM335X_CM_PER_EPWMSS2_CLKCTRL_IDLEST_SHIFT;
+    const uint32_t clkctrl = AM335X_CM_PER_ADDR + AM335X_CM_PER_EPWMSS2_CLKCTRL;
+    const uint32_t idle_bits = AM335X_CM_PER_EPWMSS2_CLKCTRL_IDLEST;
+    const uint32_t is_enable = AM335X_CM_PER_EPWMSS2_CLKCTRL_MODULEMODE_ENABLE;
+    const uint32_t module_mode = AM335X_CM_PER_EPWMSS2_CLKCTRL_MODULEMODE;
+
+    REG(clkctrl) |= is_enable;
+    while((REG(clkctrl) & module_mode) != is_enable);
+    while((REG(clkctrl) & idle_bits) != is_functional);
+    } else 
+	is_valid = false;
+  return is_valid;
 }
 
-/**
- * @brief This function intilize clock and pinmuxing for pwm sub system.
- *
- * @param PWMSS_ID It is the instance number of EPWM of pwm sub system.
- * 
- * @return true if successful
- **/
-
-bool beagle_pwm_init(uint32_t pwmss_id)
+bool beagle_pwm_init(BBB_PWMSS pwmss_id)
 {
+  const bool id_is_valid = pwmss_id < BBB_PWMSS_COUNT;
   bool status = true;
-  if(pwmss_id <3 & pwmss_id >=0) 
+  if(id_is_valid) 
   {
   module_clk_config(pwmss_id);
-  epwm_pinmux_setup(pwmss_id);
-  epwm_clock_enable(pwmss_id);
+  pwm_clock_enable(pwmss_id);	
   pwmss_tbclk_enable(pwmss_id);
-  return status;
   }
   else {
 	status =false;
-  return status;
   }
+  return status;
 }
 
-
-
-
-
-/* PWMSS setting
- *      set pulse argument of epwm module
- *
- *      @param pwm_id    : EPWMSS number , 0~2
- *      @param pwm_freq : frequency to be generated
- *      @param dutyA    : Duty Cycle in ePWM A
- *      @param dutyB    : Duty Cycle in ePWM B
- *
- *      @return         : 1 for success , 0 for failed
- *
- *      @example        :  PWMSS_Setting(0 , 50.0f , 50.0f , 25.0f);      // Generate 50HZ pwm in PWM0 ,
- *                                                                              // duty cycle is 50% for ePWM0A , 25% for ePWM0B
- *
- *      @Note :
- *              find an number nearst 65535 for TBPRD , to improve duty precision,
- *
- *              Using big TBPRD can increase the range of CMPA and CMPB ,
- *              and it means we can get better precision on duty cycle.
- *
- *              EX : 20.25% duty cycle
- *                  on TBPRD = 62500 , CMPA = 12656.25 ( .25 rejection) , real duty : 20.2496% (12656 /62500)
- *                  on TBPRD = 6250  , CMPA = 1265.625 ( .625 rejection), real duty : 20.24%   (1265 6250)
- *                  on TBPRD = 500   , CMPA = 101.25   ( .25 rejection) , real duty : 20.2%    (101/500)
- *
- *              Divisor = CLKDIV * HSPCLKDIV
- *                      1 TBPRD : 10 ns (default)
- *                      65535 TBPRD : 655350 ns
- *                      65535 TBPRD : 655350 * Divisor ns  = X TBPRD : Cyclens
- *
- *              accrooding to that , we must find a Divisor value , let X nearest 65535 .
- *              so , Divisor must  Nearest Cyclens/655350
- */
-
-int beagle_pwmss_setting(uint32_t pwm_id, float pwm_freq, float dutyA, float dutyB)
+int beagle_pwm_configure(BBB_PWMSS pwm_id, float pwm_freq, float dutyA, float dutyB)
 {	
   uint32_t baseAddr;
-  int param_error =1;
-  if(pwm_freq < 0)
-	param_error =0;
-  if(dutyA < 0.0f || dutyA > 100.0f || dutyB < 0.0f || dutyB > 100.0f)
-	param_error = 0;
-  if(param_error == 0) {
-	printf("ERROR in parameter \n");
+  int status = 1;
+  float Cycle = 0.0f,Divisor =0;
+  unsigned int i,j;
+  const float CLKDIV_div[] = {1.0,2.0,4.0,8.0,16.0,32.0,64.0,128.0};
+  const float HSPCLKDIV_div[] = {1.0, 2.0, 4.0, 6.0, 8.0, 10.0,12.0, 14.0};
+  int NearCLKDIV =7,NearHSPCLKDIV =7,NearTBPRD =0;
+	
+  if(pwm_freq <= BBB_PWM_FREQ_THRESHOLD) {
+	status =0;
+  }
+  if(dutyA < 0.0f || dutyA > 100.0f || dutyB < 0.0f || dutyB > 100.0f) {
+	status = 0;
   }
   dutyA /= 100.0f;
   dutyB /= 100.0f;
 
-  /*Compute necessary TBPRD*/
-  float Cyclens = 0.0f;
-  float Divisor =0;
-  int i,j;
-  const float CLKDIV_div[] = {1.0,2.0,4.0,8.0,16.0,32.0,64.0,128.0};
-  const float HSPCLKDIV_div[] = {1.0, 2.0, 4.0, 6.0, 8.0, 10.0,12.0, 14.0};
-  int NearCLKDIV =7;
-  int NearHSPCLKDIV =7;
-  int NearTBPRD =0;
-
+  const uint16_t clkdiv_clear = REG16(baseAddr + AM335X_EPWM_TBCTL) &
+  (~AM335X_EPWM_TBCTL_CLKDIV);
+  const uint16_t clkdiv_write = (NearCLKDIV
+  << AM335X_EPWM_TBCTL_CLKDIV_SHIFT) & AM335X_EPWM_TBCTL_CLKDIV;
+  const uint16_t hspclkdiv_clear = REG16(baseAddr + AM335X_EPWM_TBCTL) &
+  (~AM335X_EPWM_TBCTL_HSPCLKDIV);
+  const uint16_t hspclkdiv_write = (NearHSPCLKDIV <<
+  AM335X_EPWM_TBCTL_HSPCLKDIV_SHIFT) & AM335X_EPWM_TBCTL_HSPCLKDIV; 
+  const uint16_t shadow_write_clear = REG16(baseAddr + AM335X_EPWM_TBCTL) &
+  (~AM335X_EPWM_PRD_LOAD_SHADOW_MASK);
+  const uint16_t shadow_write_disable = ((bool)AM335X_EPWM_SHADOW_WRITE_DISABLE <<
+  AM335X_EPWM_TBCTL_PRDLD_SHIFT) & AM335X_EPWM_PRD_LOAD_SHADOW_MASK;
+  const uint16_t ctrmode_clear = REG16(baseAddr + AM335X_EPWM_TBCTL) &
+  (~AM335X_EPWM_COUNTER_MODE_MASK);
+  const uint16_t ctrmode_set = ((unsigned int)AM335X_EPWM_COUNT_UP <<
+  AM335X_TBCTL_CTRMODE_SHIFT) &  AM335X_EPWM_COUNTER_MODE_MASK;
+   
   /** 10^9 /Hz compute time per cycle (ns) */
-  Cyclens = 1000000000.0f / pwm_freq;
+  Cycle = 1000000000.0f / pwm_freq;
 
   /** am335x provide (128* 14) divider and per TBPRD means 10ns when divider 
     * and max TBPRD is 65535 so max cycle is 128 * 8 * 14 * 65535 * 10ns */
-  Divisor = (Cyclens / 655350.0f);
+  Divisor = (Cycle / 655350.0f);
 	
   if(Divisor > (128 * 14)) {
-	printf("Can't generate %f HZ",pwm_freq);
 	return 0;
   }
   else {
@@ -298,55 +280,42 @@ int beagle_pwmss_setting(uint32_t pwm_id, float pwm_freq, float dutyA, float dut
 			}
 		}
 	}
-  baseAddr = select_pwmss(pwm_id);	
-  REG16(baseAddr + EPWM_TBCTL) &= ~(TBCTL_CLKDIV_MASK | TBCTL_HSPCLKDIV_MASK);
+  baseAddr = select_pwm(pwm_id);	
+  REG16(baseAddr + AM335X_EPWM_TBCTL) &= ~(AM335X_TBCTL_CLKDIV_MASK | AM335X_TBCTL_HSPCLKDIV_MASK);
 			
-  REG16(baseAddr + EPWM_TBCTL) = (REG16(baseAddr + EPWM_TBCTL) &
-  (~EPWM_TBCTL_CLKDIV)) | ((NearCLKDIV 
-  << EPWM_TBCTL_CLKDIV_SHIFT) & EPWM_TBCTL_CLKDIV);
+  REG16(baseAddr + AM335X_EPWM_TBCTL) = clkdiv_clear | clkdiv_write;
 
-  REG16(baseAddr + EPWM_TBCTL) = (REG16(baseAddr + EPWM_TBCTL) &
-  (~EPWM_TBCTL_HSPCLKDIV)) | ((NearHSPCLKDIV << 
-  EPWM_TBCTL_HSPCLKDIV_SHIFT) & EPWM_TBCTL_HSPCLKDIV);
+  REG16(baseAddr + AM335X_EPWM_TBCTL) = hspclkdiv_clear | hspclkdiv_write;
 
-  NearTBPRD = (Cyclens / (10.0 * CLKDIV_div[NearCLKDIV] * HSPCLKDIV_div[NearHSPCLKDIV]));
+  NearTBPRD = (Cycle / (10.0 * CLKDIV_div[NearCLKDIV] * HSPCLKDIV_div[NearHSPCLKDIV]));
 		
-  REG16(baseAddr + EPWM_TBCTL) = (REG16(baseAddr + EPWM_TBCTL) &
-  (~EPWM_PRD_LOAD_SHADOW_MASK)) | (((bool)EPWM_SHADOW_WRITE_DISABLE <<
-  EPWM_TBCTL_PRDLD_SHIFT) & EPWM_PRD_LOAD_SHADOW_MASK);
+  REG16(baseAddr + AM335X_EPWM_TBCTL) = shadow_write_clear | shadow_write_disable;
 
-  REG16(baseAddr + EPWM_TBCTL) = (REG16(baseAddr + EPWM_TBCTL) &
-  (~EPWM_COUNTER_MODE_MASK)) | (((unsigned int)EPWM_COUNT_UP <<
-  EPWM_TBCTL_CTRMODE_SHIFT) &  EPWM_COUNTER_MODE_MASK);
+  REG16(baseAddr + AM335X_EPWM_TBCTL) = ctrmode_clear | ctrmode_set;
 
   /*setting clock divider and freeze time base*/
-  REG16(baseAddr + EPWM_CMPB) = (unsigned short)((float)(NearTBPRD) * dutyB);
-  REG16(baseAddr + EPWM_CMPA) = (unsigned short)((float)(NearTBPRD) * dutyA);
-  REG16(baseAddr + EPWM_TBPRD) = (unsigned short)NearTBPRD;
-  REG16(baseAddr + EPWM_TBCNT) = 0;
+  REG16(baseAddr + AM335X_EPWM_CMPB) = (unsigned short)((float)(NearTBPRD) * dutyB);
+  REG16(baseAddr + AM335X_EPWM_CMPA) = (unsigned short)((float)(NearTBPRD) * dutyA);
+  REG16(baseAddr + AM335X_EPWM_TBPRD) = (unsigned short)NearTBPRD;
+  REG16(baseAddr + AM335X_EPWM_TBCNT) = 0;
   }
-  return 1;
+  return status;
 }
 
-
-/**
- * @brief   This API enables the particular PWM module.
- *
- * @param   pwmid  It is the instance number of EPWM of pwm sub system.
- *
- * @return  true if successful
- *
- **/
-bool beagle_ehrpwm_enable(uint32_t pwmid)
+bool beagle_pwm_enable(BBB_PWMSS pwmid)
 {
+  const bool id_is_valid = pwmid < BBB_PWMSS_COUNT;
   bool status = true;
-  uint32_t baseAddr;
-  if(pwmid<3 & pwmid >=0) {
-  baseAddr = select_pwmss(pwmid);
-  REG16(baseAddr + EPWM_AQCTLA) = EPWM_AQCTLA_ZRO_EPWMXAHIGH | (EPWM_AQCTLA_CAU_EPWMXATOGGLE << EPWM_AQCTLA_CAU_SHIFT);
-  REG16(baseAddr + EPWM_AQCTLB) = EPWM_AQCTLB_ZRO_EPWMXBHIGH | (EPWM_AQCTLB_CBU_EPWMXBTOGGLE << EPWM_AQCTLB_CBU_SHIFT);
-  REG16(baseAddr + EPWM_TBCNT) = 0;
-  REG16(baseAddr + EPWM_TBCTL) |=  TBCTL_FREERUN  | TBCTL_CTRMODE_UP;
+  if(id_is_valid) {
+  const uint32_t baseAddr = select_pwm(pwmid);
+
+  /* Initially set EPWMxA o/p high , when increasing counter = CMPA toggle o/p of EPWMxA */
+  REG16(baseAddr + AM335X_EPWM_AQCTLA) = AM335X_EPWM_AQCTLA_ZRO_XAHIGH | (AM335X_EPWM_AQCTLA_CAU_EPWMXATOGGLE << AM335X_EPWM_AQCTLA_CAU_SHIFT);
+  /* Initially set EPWMxB o/p high , when increasing counter = CMPA toggle o/p of EPWMxB */  
+  REG16(baseAddr + AM335X_EPWM_AQCTLB) = AM335X_EPWM_AQCTLB_ZRO_XBHIGH | (AM335X_EPWM_AQCTLB_CBU_EPWMXBTOGGLE << AM335X_EPWM_AQCTLB_CBU_SHIFT);
+  REG16(baseAddr + AM335X_EPWM_TBCNT) = 0;
+  /* Set counter mode : Up-count mode */
+  REG16(baseAddr + AM335X_EPWM_TBCTL) |=  AM335X_TBCTL_FREERUN  | AM335X_TBCTL_CTRMODE_UP;
   return status;
   }
   else {
@@ -355,25 +324,16 @@ bool beagle_ehrpwm_enable(uint32_t pwmid)
   }
 }
 
-/**
- * @brief   This API disables the HR sub-module.
- *
- * @param   pwmid  It is the instance number of EPWM of pwm sub system.
- *
- * @return  true if successful
- *
- **/
-
-bool beagle_ehrpwm_disable(uint32_t pwmid)
+bool beagle_pwm_disable(BBB_PWMSS pwmid)
 {
+  const bool id_is_valid = pwmid < BBB_PWMSS_COUNT;
   bool status = true;
-  uint32_t baseAddr;
-  if(pwmid<3 & pwmid >=0) {
-  baseAddr = select_pwmss(pwmid);
-  REG16(baseAddr + EPWM_TBCTL) = EPWM_TBCTL_CTRMODE_STOPFREEZE;
-  REG16(baseAddr + EPWM_AQCTLA) = EPWM_AQCTLA_ZRO_EPWMXALOW | (EPWM_AQCTLA_CAU_EPWMXATOGGLE << EPWM_AQCTLA_CAU_SHIFT);
-  REG16(baseAddr + EPWM_AQCTLB) = EPWM_AQCTLA_ZRO_EPWMXBLOW | (EPWM_AQCTLB_CBU_EPWMXBTOGGLE << EPWM_AQCTLB_CBU_SHIFT);
-  REG16(baseAddr + EPWM_TBCNT)  = 0;
+  if(id_is_valid) {
+  const uint32_t baseAddr = select_pwm(pwmid);
+  REG16(baseAddr + AM335X_EPWM_TBCTL) = AM335X_EPWM_TBCTL_CTRMODE_STOPFREEZE;
+  REG16(baseAddr + AM335X_EPWM_AQCTLA) = AM335X_EPWM_AQCTLA_ZRO_XALOW | (AM335X_EPWM_AQCTLA_CAU_EPWMXATOGGLE << AM335X_EPWM_AQCTLA_CAU_SHIFT);
+  REG16(baseAddr + AM335X_EPWM_AQCTLB) = AM335X_EPWM_AQCTLA_ZRO_XBLOW | (AM335X_EPWM_AQCTLB_CBU_EPWMXBTOGGLE << AM335X_EPWM_AQCTLB_CBU_SHIFT);
+  REG16(baseAddr + AM335X_EPWM_TBCNT)  = 0;
   return status;
   }
   else {
@@ -382,3 +342,34 @@ bool beagle_ehrpwm_disable(uint32_t pwmid)
   }
 }
 
+#endif
+
+/* For support of BeagleboardxM */
+#if IS_DM3730
+
+/* Currently this section is just to satisfy
+ * GPIO API and to make the build successful.
+ * Later on support can be added here.
+ */
+bool beagle_pwm_init(BBB_PWMSS pwmss_id)
+{
+return false;
+}
+bool beagle_pwm_disable(BBB_PWMSS pwmid)
+{
+return false;
+}
+bool beagle_pwm_enable(BBB_PWMSS pwmid)
+{
+return false;
+}
+int beagle_pwm_configure(BBB_PWMSS pwm_id, float pwm_freq, float dutyA, float dutyB)
+{
+return -1;
+}
+bool beagle_pwm_pinmux_setup(uint32_t pin_no, BBB_PWMSS pwm_id)
+{
+return false;
+}
+
+#endif
